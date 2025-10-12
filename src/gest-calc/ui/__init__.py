22316d10de3c27e1b5268887ccf145f7b323d09d
VforVitorio <victorvegasobral@yahoo.com>
2025-10-12 20:19:23 +0200

    added init file for making ui dir a module and also the imports of components.py

---
+++ src/gest-calc/ui/__init__.py
@@ -0,0 +1,8 @@
+"""
+UI module for Sign2Calc Calculator
+
+"""
+
+from .components import Button
+
+__all__ = ["Button"]
---
+++ src/gest-calc/ui/components.py
@@ -0,0 +1,15 @@
+"""
+This file contains the UI Components for the SIgn2Calc calculator
+Contains reusable UI elements like buttons and displays
+"""
+
+import cv2
+from config import (
+    BUTTON_BG_COLOR,
+    BUTTON_BORDER_COLOR,
+    BUTTON_TEXT_COLOR,
+    BORDER_THICKNESS,
+    FONT,
+    FONT_SCALE,
+    FONT_THICKNESS
+)
